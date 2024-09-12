04e06c257b69ab805e703de2e727a90806dd6bdb
Ramesh <102596577+rameshgiri0223@users.noreply.github.com>
2024-09-13 00:08:22 +0530

    Create SuperReducedString.java

---
+++ Hacker Rank/String/SuperReducedString.java
@@ -0,0 +1,22 @@
+class Result {
+  
+    public static String superReducedString(String s) {
+    // Write your code here
+    StringBuilder sb = new StringBuilder(s);
+    for(int i = 1; i < sb.length(); i++){
+        if(sb.charAt(i) == sb.charAt(i-1)){
+            sb.delete(i-1, i+1);
+            i = 0;
+        }
+    }
+    String answer = sb.toString();
+    if(answer.length() == 0){
+        return "Empty String";
+    }
+    else {
+        return answer;
+    }
+
+    }
+
+}
